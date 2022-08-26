4c1ac331cd06b2fb5bb7c780b3f48a363476e6a4
yichenlin <yichenlin@creality.com>
2022-08-26 09:51:32 +0800

    fix[texture-mapping] make perfect
    Change-Id: Ibdfa8bd2b5da1c955a1b38e2c2b329d9465ebf06

--- enchase/trimeshgenerator.cpp
+++ enchase/trimeshgenerator.cpp
@@ -63,12 +63,26 @@ namespace enchase
 
 		input->cornerareas.resize(vertexSize);
 
-		typedef std::function<trimesh::point(const trimesh::vec3& pos)> uvFunc;
+		typedef std::function<trimesh::point(const trimesh::vec3& pos, const trimesh::vec3& nor, const trimesh::vec3& cylinderz)> uvFunc;
 		uvFunc funcs[3];
-		uvFunc cubeFunc = [](const trimesh::vec3& pos)->trimesh::point {
-			return (trimesh::vec3(1.0f, 1.0f, 0.0f) + pos) / 2.0f;
+		uvFunc cubeFunc = [](const trimesh::vec3& pos, const trimesh::vec3& nor, const trimesh::vec3& cylinderz)->trimesh::point {
+			trimesh::vec2 varyUV;
+			if (abs(nor.x) > abs(nor.y))
+			{
+				varyUV = (trimesh::vec2(1.0, 1.0) + trimesh::vec2(pos.y, pos.z)) / 2.0;
+			}
+			else if (abs(nor.y) > abs(nor.z))
+			{
+				varyUV = (trimesh::vec2(1.0, 1.0) + trimesh::vec2(pos.x, pos.z)) / 2.0;
+			}
+			else if (abs(nor.z) > abs(nor.y))
+			{
+				varyUV = (trimesh::vec2(1.0, 1.0) + trimesh::vec2(pos.x, pos.y)) / 2.0;
+			}
+				
+			return trimesh::vec3(varyUV.x,varyUV.y,0);
 		};
-		uvFunc sphereFunc = [](const trimesh::vec3& pos)->trimesh::point {
+		uvFunc sphereFunc = [](const trimesh::vec3& pos, const trimesh::vec3& nor, const trimesh::vec3& cylinderz)->trimesh::point {
 			float r = trimesh::len(pos);
 			float V = acos(pos.z / r) / M_PIf;
 			float d = pos.x / r / sin(M_PIf * V);
@@ -82,8 +96,14 @@ namespace enchase
 
 			return trimesh::vec3(U, V, 0.0f);
 		};
-		uvFunc cylinderFunc = [](const trimesh::vec3& pos)->trimesh::point {
-			return trimesh::vec3(0.0f, 0.0f, 0.0f);
+		uvFunc cylinderFunc = [](const trimesh::vec3& pos, const trimesh::vec3& nor, const trimesh::vec3& cylinder)->trimesh::point {
+			trimesh::vec2 vec(cylinder.x, cylinder.y);
+			float r = trimesh::len(vec);
+
+			float U = acos(cylinder.y / r) / M_PIf;
+			float V = cylinder.z + 0.5;
+
+			return trimesh::vec3(U, V, 0.0f);
 		};
 		funcs[0] = cubeFunc;
 		funcs[1] = sphereFunc;
@@ -98,15 +118,22 @@ namespace enchase
 		trimesh::vec3 boxSize = box.max - box.min;
 		trimesh::vec3 boxCenter = (box.max + box.min) / 2.0f;
 		float r = std::max(boxSize.z, std::max(boxSize.x, boxSize.y)) / 2.0f;
+		float rx = std:: max(boxSize.x, boxSize.y) / 2.0;
 
 		for (int i = 0; i < vertexSize; ++i)
 		{
 			if (i == 22147)
 				int j = 0;
 
-			trimesh::vec3 pos = input->vertices.at(i);
-			pos = (pos - boxCenter) / r;
-			input->cornerareas.at(i) = funcs[type](pos);
+			trimesh::vec3 pos = input->vertices.at(i)- boxCenter;
+			trimesh::vec3 nor = input->normals.at(i);
+			trimesh:: vec3 xyzOld = pos;
+			trimesh:: vec2 xy = trimesh:: vec2(pos.x, pos.y);
+			xy = xy / trimesh::vec2(rx, rx);
+			pos = pos  / r;
+			float zzzz = xyzOld.z / boxSize.z;
+			trimesh::vec3 cylinder(xy.x, xy.y, zzzz);
+			input->cornerareas.at(i) = funcs[type](pos,nor,cylinder);
 		}
 	}
 }
